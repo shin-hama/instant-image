a7962df383b63e9dd393fda9f4f557da8ab475d5
shin-hama <htysivista@gmail.com>
2022-01-20 16:56:56 +0900

    MAINT: Refactor to separeta logic of uploading image as custom hook

--- src/features/canvas/components/UploadButton.tsx
+++ src/features/canvas/components/UploadButton.tsx
@@ -2,59 +2,17 @@ import * as React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import Fab from '@mui/material/Fab'
-import { Image as KonvaImage } from 'react-konva'
-import { useShapes } from '../contexts/ShapesProvider'
 import { useWindowSize } from 'features/canvas/hooks/useWindowSize'
+import { useImageLoader } from 'features/canvas/hooks/useImageLoader'
 
 const labelId = 'upload-image-button'
 
 const UploadButton = () => {
-  const [, actions] = useShapes()
   const windowSize = useWindowSize()
-  const [maxWidth, setMaxWidth] = React.useState(0)
-  const [maxHeight, setMaxHeight] = React.useState(0)
-
-  React.useEffect(() => {
-    setMaxWidth(windowSize.width * 0.6)
-    setMaxHeight(windowSize.height * 0.6)
-  }, [windowSize])
-
-  const loadImage = (file: File) => {
-    console.log(file)
-    const url = URL.createObjectURL(file)
-    const image = new window.Image()
-    image.src = url
-
-    image.onload = () => {
-      console.log(windowSize)
-      const size = {
-        width: image.width,
-        height: image.height,
-      }
-      // Canvas のサイズを超えている場合は、Canvas をはみ出ないサイズに調整
-      if (size.width > maxWidth || size.height > maxHeight) {
-        if (size.width > size.height) {
-          size.height = (maxWidth * size.height) / size.width
-          size.width = maxWidth
-        } else {
-          size.width = (maxHeight * size.width) / size.height
-          size.height = maxHeight
-        }
-      }
-
-      actions.push(
-        <KonvaImage
-          draggable
-          image={image}
-          x={200}
-          y={200}
-          width={size.width}
-          height={size.height}
-        />
-      )
-      URL.revokeObjectURL(url)
-    }
-  }
+  const loadImage = useImageLoader(
+    windowSize.width * 0.6,
+    windowSize.height * 0.6
+  )
 
   const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = Array.from(e.target.files || [])
