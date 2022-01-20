87c9fa0e46470b64249d670a461b4a190ff81fcd
shin-hama <htysivista@gmail.com>
2022-01-20 12:51:20 +0900

    FEAT: Add close button on right top of config editor

--- src/features/config/components/ConfigEditor.tsx
+++ src/features/config/components/ConfigEditor.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { styled } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import Drawer from '@mui/material/Drawer'
+import Fab from '@mui/material/Fab'
 import Toolbar from '@mui/material/Toolbar'
 
 import LineConfigEditor from '../components/LineConfig'
@@ -12,6 +16,20 @@ const Editors = {
 }
 export type EditorType = keyof typeof Editors
 
+const TransparentFab = styled(Fab)(({ theme }) => ({
+  position: 'absolute',
+  top: Number.parseInt(theme.mixins.toolbar.minHeight?.toString() || '0') + 8,
+  right: 8,
+  boxShadow: 'none',
+  backgroundColor: 'transparent',
+  '&:hover': {
+    backgroundColor: 'transparent',
+  },
+  '&:active': {
+    boxShadow: 'none',
+  },
+}))
+
 type Props = {
   editor?: EditorType
   onClose: () => void
@@ -21,10 +39,18 @@ const ConfigEditor: React.FC<Props> = ({ editor, onClose }) => {
     <Drawer
       open={Boolean(editor)}
       anchor="right"
-      variant="permanent"
+      variant="persistent"
       onClose={onClose}>
       <Toolbar />
       <Box sx={{ margin: 2 }}>{editor ? Editors[editor] : <></>}</Box>
+      <TransparentFab
+        disableRipple
+        disableFocusRipple
+        disableTouchRipple
+        variant="extended"
+        onClick={onClose}>
+        <FontAwesomeIcon icon={faTimes} />
+      </TransparentFab>
     </Drawer>
   )
 }
