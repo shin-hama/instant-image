1a0a51973655c75dda90b253e02ebe0769636def
shin-hama <htysivista@gmail.com>
2022-01-25 07:28:57 +0900

    FEAT: Make tool panel width to be 100% of window size

--- src/features/canvas/components/ToolPanel.tsx
+++ src/features/canvas/components/ToolPanel.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react'
-import Box from "@mui/material/Box"
-import Button from "@mui/material/Button"
-import Stack from "@mui/material/Stack"
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
 
 const ToolPanel = () => {
   return (
-    <Box>
-      <Stack direction="row" spacing={2}>
-        <Button>Hoge</Button>
-        <Button>Bar</Button>
-        <Button>Upload</Button>
-      </Stack>
+    <Box sx={{ width: '100vw' }}>
+      <Box sx={{ width: '100%', overflow: 'auto' }}>
+        <Stack direction="row" spacing={2}>
+          <Button>Sample</Button>
+          <Button>Sample</Button>
+          <Button>Sample</Button>
+          <Button>Sample</Button>
+          <Button>Sample</Button>
+          <Button>Sample</Button>
+        </Stack>
+      </Box>
     </Box>
   )
 }
